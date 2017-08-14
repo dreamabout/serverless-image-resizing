c1d8c9a0062d45559814dc631e8df7689a0513af
Thomas Faurbye Nielsen <archanium@gmail.com>
2017-08-14 09:50:11 +0200

    Update lambda for our versioning needs

--- lambda/index.js
+++ lambda/index.js
@@ -11,17 +11,24 @@ const URL = process.env.URL;
 
 exports.handler = function(event, context, callback) {
   const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
-
+  const match = key.match(new RegEx(
+	  "^/(?<shopId>\d{1,3})/((?<version>\d{1})?/?)(images/)?(?<folder>products|blocks)/(?<width>\d{1,4})/(?<height>\d{1,4})/(?<path>[\w\.\-]+)$"));
+  const width = parseInt(match["width"], 10);
+  const height = parseInt(match["height"], 10);
+  const path = match["path"];
+  const folder = match["folder"];
+  const originalKey = "catalog/" + folder + "/images/" + path;
+  let version = parseInt(match["version"] || 1, 10);
+  let resizeFunc = (data) => {
+    return Sharp(data.body).resize(width || null, height || null).png().toBuffer()
+  }
+  if (version === 2) {
+    resizeFunc = (data) => {
+      return Sharp(data.body).resize(width, height).embed().png().toBuffer();
+    } 
+  }
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
+    .then(resizeFunc)
     .then(buffer => S3.putObject({
         Body: buffer,
         Bucket: BUCKET,
